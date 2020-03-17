2e152b5c5a1a83d1f2aecb1f3bcba0bd775e2670
rvadhith <61987696+rvadhith@users.noreply.github.com>
2020-03-17 14:15:43 +0530

    Add files via upload

---
+++ project-12-superwars-basic-master/src/app.js
@@ -0,0 +1,69 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// initialize players with image and strength
+
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+    for (i = 0; i < players.length; i++) {
+        detailedPlayers[i] = {
+            name: players[i],
+            image: "images/super-" + (i + 1) + ".png",
+            strength: getRandomStrength(),
+            type: (i % 2 == 0) ? "hero" : "villain"
+        }
+    }
+    return detailedPlayers;
+}
+
+// getting random strength
+const getRandomStrength = () => {
+    return Math.ceil(Math.random() * 100);
+}
+
+const buildPlayers = (players, type) => {
+    let fragment = '';
+
+    for (i = 0; i < players.length; i++) {
+        player = `<div class="player">
+                    <img src="${players[i].image}" alt="">
+                    <div class="name">${players[i].name}</div>
+                    <div class="strength">${players[i].strength}</div>
+                </div>`;
+        if (players[i].type == type){
+            fragment = fragment + player;
+        }    
+    }
+    return fragment;
+}
+// Display players in HTML
+const viewPlayers = (players) => {
+
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+
+}
+
+window.onload = () => {
+    viewPlayers(initPlayers(PLAYERS));
+}
